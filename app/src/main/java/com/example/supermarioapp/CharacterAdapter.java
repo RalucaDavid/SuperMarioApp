bb316fbb4a1006c9a43593479677d5d58d1d59a5
RalucaDavid <117584603+RalucaDavid@users.noreply.github.com>
2025-04-02 21:56:27 +0300

    add the activy classes and the character adapter class

---
+++ app/src/main/java/com/example/supermarioapp/CharacterAdapter.java
@@ -0,0 +1,50 @@
+package com.example.supermarioapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class CharacterAdapter extends RecyclerView.Adapter<CharacterAdapter.ViewHolder> {
+    private List<Character> characters;
+    private Context context;
+
+    public CharacterAdapter(List<Character> characters, Context context) {
+        this.characters = characters;
+        this.context = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.character_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Character character = characters.get(position);
+        holder.name.setText(character.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return characters.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView name;
+        ImageView image;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.characterName);
+            image = itemView.findViewById(R.id.characterImage);
+        }
+    }
+}
